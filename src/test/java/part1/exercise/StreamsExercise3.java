f4a004a5c418f1736b8634fe19fd2ad2710cb759
Konstantin Mavropulo <konstantin.n.mavropulo@gmail.com>
2018-01-23 01:29:16 +0300

    part1_Mavropulo_Konstantin: StreamExercise3 has been completed.

--- src/test/java/part1/exercise/StreamsExercise3.java
+++ src/test/java/part1/exercise/StreamsExercise3.java
@@ -4,7 +4,14 @@ import data.WNPResult;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.ToLongFunction;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
@@ -13,17 +20,32 @@ public class StreamsExercise3 {
 
     @Test
     public void warAndPeace() throws IOException {
-        Stream.of(
+        Map<String, Long> wordCount = Stream.of(
                 Paths.get("WAP12.txt"),
                 Paths.get("WAP34.txt")
-                );
+        )
+                .flatMap(p -> {
+                    try {
+                        return Files.lines(p, Charset.forName("windows-1251"));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                })
+                .parallel()
+                .map(String::toLowerCase)
+                .map(l -> l.split("[^\\p{javaLowerCase}]+"))
+                .flatMap(a -> Stream.of(a).parallel())
+                .filter(w -> w.length() >= 4)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
 
-
-        String result = null;
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
+        String result = wordCount.entrySet().stream().parallel()
+                .filter(e -> e.getValue() >= 10)
+                .sorted(Comparator.comparingLong((ToLongFunction<Map.Entry<String, Long>>) Map.Entry::getValue)
+                        .reversed()
+                        .thenComparing(Comparator.comparing(Map.Entry::getKey)))
+                .map(e -> e.getKey().concat(" - ").concat(e.getValue().toString()))
+                .collect(Collectors.joining("\n"));
 
         assertEquals(new WNPResult().result, result);
     }
