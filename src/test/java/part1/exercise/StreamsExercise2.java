df28356c3158513a4514de8dfe5ad133f0d6503f
Aleksei_Parshin <Aleksei_Parshin@epam.com>
2018-01-23 20:05:19 +0300

    ex1 and half of ex2

--- src/test/java/part1/exercise/StreamsExercise2.java
+++ src/test/java/part1/exercise/StreamsExercise2.java
@@ -5,6 +5,9 @@ import com.google.common.collect.ImmutableMap;
 import data.Employee;
 import data.JobHistoryEntry;
 import data.Person;
+import java.util.Comparator;
+import java.util.Objects;
+import java.util.stream.Collectors;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -12,23 +15,68 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.collectingAndThen;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.maxBy;
+import static java.util.stream.Collectors.toMap;
+import static java.util.stream.Collectors.toSet;
 import static org.junit.Assert.assertEquals;
 
 public class StreamsExercise2 {
-    // https://youtu.be/kxgo7Y4cdA8 Сергей Куксенко и Алексей Шипилёв — Через тернии к лямбдам, часть 1
-    // https://youtu.be/JRBWBJ6S4aU Сергей Куксенко и Алексей Шипилёв — Через тернии к лямбдам, часть 2
+  // https://youtu.be/kxgo7Y4cdA8 Сергей Куксенко и Алексей Шипилёв — Через тернии к лямбдам, часть 1
+  // https://youtu.be/JRBWBJ6S4aU Сергей Куксенко и Алексей Шипилёв — Через тернии к лямбдам, часть 2
 
-    // https://youtu.be/O8oN4KSZEXE Сергей Куксенко — Stream API, часть 1
-    // https://youtu.be/i0Jr2l3jrDA Сергей Куксенко — Stream API, часть 2
+  // https://youtu.be/O8oN4KSZEXE Сергей Куксенко — Stream API, часть 1
+  // https://youtu.be/i0Jr2l3jrDA Сергей Куксенко — Stream API, часть 2
 
-    // TODO class PersonEmployerPair
 
-    @Test
-    public void employersStuffLists() {
-        final List<Employee> employees = getEmployees();
+  class PersonEmployerPair {
+    private final Person person;
+    private final String employer;
 
-        Map<String, List<Person>> employersStuffLists = null;
-        // TODO map employer vs persons with job history related to it
+    public PersonEmployerPair(Person person, String employer) {
+      this.person = person;
+      this.employer = employer;
+    }
+
+    public Person getPerson() {
+      return person;
+    }
+
+    public String getEmployer() {
+      return employer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      PersonEmployerPair that = (PersonEmployerPair) o;
+      return Objects.equals(person, that.person) &&
+          Objects.equals(employer, that.employer);
+    }
+
+    @Override
+    public int hashCode() {
+
+      return Objects.hash(person, employer);
+    }
+  }
+
+  @Test
+  public void employersStuffLists() {
+    final List<Employee> employees = getEmployees();
+
+    Map<String, List<Person>> employersStuffLists = employees.stream()
+        .flatMap(employee -> employee.getJobHistory().stream()
+            .map(jobHistoryEntry -> new PersonEmployerPair(employee.getPerson(), jobHistoryEntry.getEmployer())))
+        .collect(Collectors.groupingBy(PersonEmployerPair::getEmployer,
+            Collectors.mapping(PersonEmployerPair::getPerson, Collectors.toList())));
 
         assertEquals(getExpectedEmployersStuffLists(), employersStuffLists);
     }
@@ -37,159 +85,168 @@ public class StreamsExercise2 {
     public void indexByFirstEmployer() {
         final List<Employee> employees = getEmployees();
 
-        Map<String, List<Person>> employeesIndex = null;
-        // TODO map employer vs persons with first job history related to it
+    Map<String, List<Person>> employeesIndex = employees.stream()
+        .flatMap(employee -> employee.getJobHistory().stream()
+            .map(jobHistoryEntry -> new PersonEmployerPair(employee.getPerson(), employee.getJobHistory().get(0).getEmployer()))
+            .distinct())
+        .collect(Collectors.groupingBy(PersonEmployerPair::getEmployer,
+            Collectors.mapping(PersonEmployerPair::getPerson, Collectors.toList())));
 
-        assertEquals(getExpectedEmployeesIndexByFirstEmployer(), employeesIndex);
+    // TODO map employer vs persons with first job history related to it
 
-    }
+    assertEquals(getExpectedEmployeesIndexByFirstEmployer(), employeesIndex);
 
-    @Test
-    public void greatestExperiencePerEmployer() {
-        Map<String, Person> employeesIndex = null;
-        // TODO map employer vs person with greatest duration in it
+  }
 
-        assertEquals(new Person("John", "White", 28), employeesIndex.get("epam"));
-    }
+  @Test
+  public void greatestExperiencePerEmployer() {
+    Map<String, Person> employeesIndex = getEmployees().stream().flatMap(employee -> employee.getJobHistory().stream()
+        .map(jobHistoryEntry -> new PersonEmployerPair(employee.getPerson(), jobHistoryEntry.getEmployer())))
+        .collect(Collectors.groupingBy(PersonEmployerPair::getEmployer,collectingAndThen(
+            maxBy(comparing(PersonPositionDuration::getDuration)), p -> p.get().getPerson())));
+    // TODO map employer vs person with greatest duration in it
+
+    assertEquals(new Person("John", "White", 28), employeesIndex.get("epam"));
+  }
 
 
-    private List<Employee> getEmployees() {
-        return Arrays.asList(
-                new Employee(
-                        new Person("John", "Galt", 20),
-                        Arrays.asList(
-                                new JobHistoryEntry(3, "dev", "epam"),
-                                new JobHistoryEntry(2, "dev", "google")
-                        )),
-                new Employee(
-                        new Person("John", "Doe", 21),
-                        Arrays.asList(
-                                new JobHistoryEntry(4, "BA", "yandex"),
-                                new JobHistoryEntry(2, "QA", "epam"),
-                                new JobHistoryEntry(2, "dev", "abc")
-                        )),
-                new Employee(
-                        new Person("John", "White", 22),
-                        Collections.singletonList(
-                                new JobHistoryEntry(6, "QA", "epam")
-                        )),
-                new Employee(
-                        new Person("John", "Galt", 23),
-                        Arrays.asList(
-                                new JobHistoryEntry(3, "dev", "epam"),
-                                new JobHistoryEntry(2, "dev", "google")
-                        )),
-                new Employee(
-                        new Person("John", "Doe", 24),
-                        Arrays.asList(
-                                new JobHistoryEntry(4, "QA", "yandex"),
-                                new JobHistoryEntry(2, "BA", "epam"),
-                                new JobHistoryEntry(2, "dev", "abc")
-                        )),
-                new Employee(
-                        new Person("John", "White", 25),
-                        Collections.singletonList(
-                                new JobHistoryEntry(6, "QA", "epam")
-                        )),
-                new Employee(
-                        new Person("John", "Galt", 26),
-                        Arrays.asList(
-                                new JobHistoryEntry(3, "dev", "epam"),
-                                new JobHistoryEntry(1, "dev", "google")
-                        )),
-                new Employee(
-                        new Person("Bob", "Doe", 27),
-                        Arrays.asList(
-                                new JobHistoryEntry(4, "QA", "yandex"),
-                                new JobHistoryEntry(2, "QA", "epam"),
-                                new JobHistoryEntry(2, "dev", "abc")
-                        )),
-                new Employee(
-                        new Person("John", "White", 28),
-                        Collections.singletonList(
-                                new JobHistoryEntry(666, "BA", "epam")
-                        )),
-                new Employee(
-                        new Person("John", "Galt", 29),
-                        Arrays.asList(
-                                new JobHistoryEntry(3, "dev", "epam"),
-                                new JobHistoryEntry(1, "dev", "google")
-                        )),
-                new Employee(
-                        new Person("John", "Doe", 30),
-                        Arrays.asList(
-                                new JobHistoryEntry(4, "QA", "yandex"),
-                                new JobHistoryEntry(2, "QA", "epam"),
-                                new JobHistoryEntry(5, "dev", "abc")
-                        )),
-                new Employee(
-                        new Person("Bob", "White", 31),
-                        Collections.singletonList(
-                                new JobHistoryEntry(6, "QA", "epam")
-                        ))
-        );
-    }
+  private List<Employee> getEmployees() {
+    return Arrays.asList(
+        new Employee(
+            new Person("John", "Galt", 20),
+            Arrays.asList(
+                new JobHistoryEntry(3, "dev", "epam"),
+                new JobHistoryEntry(2, "dev", "google")
+            )),
+        new Employee(
+            new Person("John", "Doe", 21),
+            Arrays.asList(
+                new JobHistoryEntry(4, "BA", "yandex"),
+                new JobHistoryEntry(2, "QA", "epam"),
+                new JobHistoryEntry(2, "dev", "abc")
+            )),
+        new Employee(
+            new Person("John", "White", 22),
+            Collections.singletonList(
+                new JobHistoryEntry(6, "QA", "epam")
+            )),
+        new Employee(
+            new Person("John", "Galt", 23),
+            Arrays.asList(
+                new JobHistoryEntry(3, "dev", "epam"),
+                new JobHistoryEntry(2, "dev", "google")
+            )),
+        new Employee(
+            new Person("John", "Doe", 24),
+            Arrays.asList(
+                new JobHistoryEntry(4, "QA", "yandex"),
+                new JobHistoryEntry(2, "BA", "epam"),
+                new JobHistoryEntry(2, "dev", "abc")
+            )),
+        new Employee(
+            new Person("John", "White", 25),
+            Collections.singletonList(
+                new JobHistoryEntry(6, "QA", "epam")
+            )),
+        new Employee(
+            new Person("John", "Galt", 26),
+            Arrays.asList(
+                new JobHistoryEntry(3, "dev", "epam"),
+                new JobHistoryEntry(1, "dev", "google")
+            )),
+        new Employee(
+            new Person("Bob", "Doe", 27),
+            Arrays.asList(
+                new JobHistoryEntry(4, "QA", "yandex"),
+                new JobHistoryEntry(2, "QA", "epam"),
+                new JobHistoryEntry(2, "dev", "abc")
+            )),
+        new Employee(
+            new Person("John", "White", 28),
+            Collections.singletonList(
+                new JobHistoryEntry(666, "BA", "epam")
+            )),
+        new Employee(
+            new Person("John", "Galt", 29),
+            Arrays.asList(
+                new JobHistoryEntry(3, "dev", "epam"),
+                new JobHistoryEntry(1, "dev", "google")
+            )),
+        new Employee(
+            new Person("John", "Doe", 30),
+            Arrays.asList(
+                new JobHistoryEntry(4, "QA", "yandex"),
+                new JobHistoryEntry(2, "QA", "epam"),
+                new JobHistoryEntry(5, "dev", "abc")
+            )),
+        new Employee(
+            new Person("Bob", "White", 31),
+            Collections.singletonList(
+                new JobHistoryEntry(6, "QA", "epam")
+            ))
+    );
+  }
 
-    private static Map<String, List<Person>> getExpectedEmployersStuffLists() {
-        Map<String, List<Person>> map = ImmutableMap.of(
-                "abc", ImmutableList.of(
-                        new Person("John", "Doe", 21),
-                        new Person("John", "Doe", 24),
-                        new Person("Bob", "Doe", 27),
-                        new Person("John", "Doe", 30)
-                ),
-                "yandex", ImmutableList.of(
-                        new Person("John", "Doe", 21),
-                        new Person("John", "Doe", 24),
-                        new Person("Bob", "Doe", 27),
-                        new Person("John", "Doe", 30)
-                ),
-                "epam", ImmutableList.of(
-                        new Person("John", "Galt", 20),
-                        new Person("John", "Doe", 21),
-                        new Person("John", "White", 22),
-                        new Person("John", "Galt", 23),
-                        new Person("John", "Doe", 24),
-                        new Person("John", "White", 25),
-                        new Person("John", "Galt", 26),
-                        new Person("Bob", "Doe", 27),
-                        new Person("John", "White", 28),
-                        new Person("John", "Galt", 29),
-                        new Person("John", "Doe", 30),
-                        new Person("Bob", "White", 31)
-                ),
-                "google", ImmutableList.of(
-                        new Person("John", "Galt", 20),
-                        new Person("John", "Galt", 23),
-                        new Person("John", "Galt", 26),
-                        new Person("John", "Galt", 29)
-                )
-        );
+  private static Map<String, List<Person>> getExpectedEmployersStuffLists() {
+    Map<String, List<Person>> map = ImmutableMap.of(
+        "abc", ImmutableList.of(
+            new Person("John", "Doe", 21),
+            new Person("John", "Doe", 24),
+            new Person("Bob", "Doe", 27),
+            new Person("John", "Doe", 30)
+        ),
+        "yandex", ImmutableList.of(
+            new Person("John", "Doe", 21),
+            new Person("John", "Doe", 24),
+            new Person("Bob", "Doe", 27),
+            new Person("John", "Doe", 30)
+        ),
+        "epam", ImmutableList.of(
+            new Person("John", "Galt", 20),
+            new Person("John", "Doe", 21),
+            new Person("John", "White", 22),
+            new Person("John", "Galt", 23),
+            new Person("John", "Doe", 24),
+            new Person("John", "White", 25),
+            new Person("John", "Galt", 26),
+            new Person("Bob", "Doe", 27),
+            new Person("John", "White", 28),
+            new Person("John", "Galt", 29),
+            new Person("John", "Doe", 30),
+            new Person("Bob", "White", 31)
+        ),
+        "google", ImmutableList.of(
+            new Person("John", "Galt", 20),
+            new Person("John", "Galt", 23),
+            new Person("John", "Galt", 26),
+            new Person("John", "Galt", 29)
+        )
+    );
 
-        return map;
-    }
+    return map;
+  }
 
-    private static Map<String, List<Person>> getExpectedEmployeesIndexByFirstEmployer() {
-        Map<String, List<Person>> map = ImmutableMap.of(
-                "yandex", ImmutableList.of(
-                        new Person("John", "Doe", 21),
-                        new Person("John", "Doe", 24),
-                        new Person("Bob", "Doe", 27),
-                        new Person("John", "Doe", 30)
-                ),
-                "epam", ImmutableList.of(
-                        new Person("John", "Galt", 20),
-                        new Person("John", "White", 22),
-                        new Person("John", "Galt", 23),
-                        new Person("John", "White", 25),
-                        new Person("John", "Galt", 26),
-                        new Person("John", "White", 28),
-                        new Person("John", "Galt", 29),
-                        new Person("Bob", "White", 31)
-                )
-        );
+  private static Map<String, List<Person>> getExpectedEmployeesIndexByFirstEmployer() {
+    Map<String, List<Person>> map = ImmutableMap.of(
+        "yandex", ImmutableList.of(
+            new Person("John", "Doe", 21),
+            new Person("John", "Doe", 24),
+            new Person("Bob", "Doe", 27),
+            new Person("John", "Doe", 30)
+        ),
+        "epam", ImmutableList.of(
+            new Person("John", "Galt", 20),
+            new Person("John", "White", 22),
+            new Person("John", "Galt", 23),
+            new Person("John", "White", 25),
+            new Person("John", "Galt", 26),
+            new Person("John", "White", 28),
+            new Person("John", "Galt", 29),
+            new Person("Bob", "White", 31)
+        )
+    );
 
-        return map;
-    }
+    return map;
+  }
 
 }
