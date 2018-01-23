405376a166808a39c7f5e68aaa48b0e7be21ce9a
Aleksandr Sakhnenko <Aleksandr_Sakhnenko@epam.com>
2018-01-23 21:13:11 +0300

    ex3 is done

--- src/test/java/part1/exercise/StreamsExercise3.java
+++ src/test/java/part1/exercise/StreamsExercise3.java
@@ -1,30 +1,66 @@
 package part1.exercise;
 
 import data.WNPResult;
+import javafx.util.Pair;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
+
+class Entry {
+    String word;
+    int count;
+
+    public Entry(String word, int count) {
+        this.word = word;
+        this.count = count;
+    }
+}
 
 public class StreamsExercise3 {
 
     @Test
     public void warAndPeace() throws IOException {
-        Stream.of(
+        Stream<Path> pathStream = Stream.of(
                 Paths.get("WAP12.txt"),
                 Paths.get("WAP34.txt")
-                );
+        );
 
+        String result = pathStream
+                .flatMap(path -> {
+                    try {
+                        return Files.lines(path, Charset.forName("windows-1251"));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                })
+                .map(line -> line.toLowerCase().trim().replaceAll("[^A-Za-zа-яА-Я]+", " "))
+                .flatMap(line -> Arrays.stream(line.split(" ")))
+                .filter(s -> s.length() >= 4)
+                .map(s -> new Pair<String, Integer>(s, 1))
+                .collect(Collectors.collectingAndThen(
+                        Collectors.groupingBy(
+                                Pair::getKey,
+                                Collectors.counting()
+                        ),
+                        map -> map.entrySet().stream()
+                                .filter(entry -> entry.getValue() >= 10)
+                                .sorted((e1, e2) -> {
+                                    int rez = e2.getValue().compareTo(e1.getValue());
+                                    return rez != 0 ? rez : e1.getKey().compareTo(e2.getKey());
 
-        String result = null;
-        // TODO map lowercased words to its amount in text and concatenate its entries.
-        // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
-        // TODO Entries in final String should be also sorted by amount and then in alphabetical order if needed.
-        // TODO Also omit any word with lengths less than 4 and frequency less than 10
-
+                                })
+                                .map(entry -> entry.getKey() + " - " + entry.getValue())
+                                .collect(Collectors.joining("\n"))
+                ));
         assertEquals(new WNPResult().result, result);
     }
 
