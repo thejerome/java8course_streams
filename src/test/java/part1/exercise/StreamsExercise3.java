6907bd56559a9bd2051c8c9f75987e61fb63d4bf
Sergei Titov <Sergei_Titov@epam.com>
2018-01-30 16:25:12 +0300

    StreamsExercise3 done

--- src/test/java/part1/exercise/StreamsExercise3.java
+++ src/test/java/part1/exercise/StreamsExercise3.java
@@ -8,6 +8,9 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
@@ -16,7 +19,7 @@ public class StreamsExercise3 {
 
     @Test
     public void warAndPeace() throws IOException {
-        String result = null; Stream.of(
+        String result = Stream.of(
                 Paths.get("WAP12.txt"),
                 Paths.get("WAP34.txt")
         )
@@ -27,8 +30,23 @@ public class StreamsExercise3 {
                         throw new RuntimeException(e);
                     }})
                 .map(line -> line.split("[^а-яА-Яa-zA-Z]"))
-                .flatMap(Arrays::stream);
-
+                .flatMap(Arrays::stream)
+                .filter(w -> w.length() >= 4)
+                .map(String::toLowerCase)
+                .collect(
+                        HashMap<String, Integer>::new,
+                        (map, w) -> map.put(w, map.computeIfAbsent(w, key -> (0)) + 1),
+                        Map::putAll
+                ).entrySet().stream()
+                .filter( e -> e.getValue() >= 10)
+                .sorted( (e1, e2) -> {
+                    if(Integer.compare(e1.getValue(), e2.getValue()) == 0)
+                        return e1.getKey().compareTo(e2.getKey());
+                    else
+                        return Long.compare(e2.getValue(), e1.getValue());
+                })
+                .map(entry -> entry.getKey() + " - " + entry.getValue())
+                .collect(Collectors.joining("\n"));
 
         // TODO map lowercased words to its amount in text and concatenate its entries.
         // TODO If word "котик" occurred in text 23 times then its entry would be "котик - 23\n".
