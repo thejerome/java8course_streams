315118009099d98833b9c4fa4ca3eef74af5214c
razyke <waqk@mail.ru>
2018-02-02 21:36:47 +0300

    Sorry, i think that we need to see if instance of it's string.

--- src/test/java/part3/exercise/lambda/LambdaExercise.java
+++ src/test/java/part3/exercise/lambda/LambdaExercise.java
@@ -85,7 +85,7 @@ public class LambdaExercise {
 
         assertEquals("John", getPersonName.apply(person));
 
-        final Predicate<String> isJohnString = String.class::isInstance; // TODO using method reference check that "John" equals string parameter
+        final Predicate<String> isJohnString = "John"::equals; // TODO using method reference check that "John" equals string parameter
 
         final Predicate<Person> isJohnPerson = combine(getPersonName, isJohnString);
 
